a0065455e503257b9d85d87eb306457fa45c4df6
peachey <>
2004-05-03 15:06:23 +0000

    New event binning package.

---
+++ src/mainpage.h
@@ -0,0 +1,9 @@
+/**
+    \mainpage evtbin package
+
+    \author  Yasushi Ikebe ikebe@milkyway.gsfc.nasa.gov
+             James Peachey peachey@lheamail.gsfc.nasa.gov
+
+    \section intro Introduction
+    Event binning tool.
+*/
