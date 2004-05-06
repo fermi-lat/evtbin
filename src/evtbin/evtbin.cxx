aa7057d16247c0abc4db4e992c27ffa6176b98f1
peachey <>
2004-05-06 15:16:30 +0000

    Add functionality for simple spectrum with logarithmic bins.

--- src/evtbin/evtbin.cxx
+++ src/evtbin/evtbin.cxx
@@ -22,8 +22,9 @@
 // Table access from tip.
 #include "tip/Table.h"
 
-// Use simple equal linear interval binner:
+// Binners used:
 #include "evtbin/TipEqualLinearBinner.h"
+#include "evtbin/TipEqualLogBinner.h"
 
 /** \class EvtBin
     \brief Application singleton for evtbin.
@@ -46,7 +47,47 @@ class EvtBin : public st_app::StApp {
 
       std::string algorithm = pars["algorithm"];
       if (0 == algorithm.compare("LC")) makeLightCurve(pars);
+      else if (0 == algorithm.compare("SPEC")) makeSimpleSpectrum(pars);
       else throw std::logic_error("Only Light Curve is currently supported");
+    }
+
+    void makeSimpleSpectrum(const st_app::AppParGroup & pars) {
+      std::string event_file = pars["eventfile"];
+      std::string out_file = pars["outfile"];
+      
+      // Get the IFileSvc singleton and use it to create the output file:
+      // tip == namespace, IFileSvc == class, instance() is static method returning the singleton
+      // createFile is a normal method:
+      tip::IFileSvc::instance().createFile(out_file, "LatSingleBinnedTemplate");
+ 
+      // Open input file for reading only:
+      const tip::Table * table = tip::IFileSvc::instance().readTable(event_file, "EVENTS");
+
+      // Now get ranges:
+      double energy_min = pars["emin"];
+      double energy_max = pars["emax"];
+      long enum_bins = pars["enumbins"];
+
+      // Make sure a positive number of bins was entered.
+      if (0 >= enum_bins) throw std::runtime_error("Number of bins (enumbins parameter) must be positive");
+
+      // Create binner object:
+      TipEqualLogBinner binner("ENERGY", "CHANNEL", "COUNTS", energy_min, energy_max, (unsigned long)(enum_bins));
+
+      // Fill the histogram:
+      binner.fillBins(table->begin(), table->end());
+
+      // Open output table for writing:
+      tip::Table * out_table = tip::IFileSvc::instance().editTable(out_file, "SPECTRUM");
+
+      // Write the output table:
+      binner.writeHistogram(out_table);
+
+// TODO:
+// 1. Energy units: Xspec needs keV, input is MeV but could be anything (read keyword)
+      delete out_table;
+
+      delete table;
     }
 
     void makeLightCurve(const st_app::AppParGroup & pars) {
@@ -58,14 +99,10 @@ class EvtBin : public st_app::StApp {
       // createFile is a normal method:
       tip::IFileSvc::instance().createFile(out_file, "LatLightCurveTemplate");
  
-      // This is the same as:
-      // tip::IFileSvc & the_service = tip::IFileSvc::instance();
-      // the_service.createFile("LC1.lc", "LatLightCurveTemplate");
-
       // Open input file for reading only:
       const tip::Table * table = tip::IFileSvc::instance().readTable(event_file, "EVENTS");
 
-      // Now make a functor to bin this vector:
+      // Now get ranges:
       double tstart = pars["tstart"];
       double tstop = pars["tstop"];
       double width = pars["deltatime"];
@@ -82,6 +119,12 @@ class EvtBin : public st_app::StApp {
       // Write the output table:
       binner.writeHistogram(out_table);
 
+// TODO for light curve:
+// 1. Copy GTI from event file (anding with the range [tstart, tstop])
+// 2. Get bin defs (non const) from timebinalg or timebinfile
+// 3. Need different template which has no TIME_DEL column in uniform case.
+// 4. Livetime (applies for all binners) from ft2 file gets written in header of histogram.
+//    Recompute iff GTI changed (in step 1).
       delete out_table;
 
       delete table;
