87dd9b16d6ac1052f037297e3cac847741833ab3
peachey <>
2004-05-12 12:53:51 +0000

    Add abstraction for intervals, and methods to access the intervals in binners.

--- src/LinearBinner.cxx
+++ src/LinearBinner.cxx
@@ -19,4 +19,19 @@ namespace evtbin {
 
   long LinearBinner::getNumBins() const { return m_num_bins; }
 
+  Binner::Interval LinearBinner::getInterval(long index) const {
+    // Check bounds, and handle endpoints explicitly to avoid any round-off:
+    if (index < 0 || index >= m_num_bins)
+      return Binner::Interval(0., 0.);
+    else if (index == 0)
+      return Binner::Interval(m_interval_begin, m_interval_begin + (1 + index) * (m_interval_end - m_interval_begin) / m_num_bins);
+    else if (index == m_num_bins - 1)
+      return Binner::Interval(m_interval_begin + index * (m_interval_end - m_interval_begin) / m_num_bins, m_interval_end);
+
+    return Binner::Interval(
+      m_interval_begin + index * (m_interval_end - m_interval_begin) / m_num_bins,
+      m_interval_begin + (1 + index) * (m_interval_end - m_interval_begin) / m_num_bins
+    );
+  }
+
 }
