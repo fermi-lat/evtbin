978b3bc0ed1160425d0302ed4713cddea76a0343
peachey <>
2004-05-13 16:52:36 +0000

    Convert to use Binner and Hist subclasses in lieu of TipBinner hierarchy.

--- src/evtbin/evtbin.cxx
+++ src/evtbin/evtbin.cxx
@@ -4,11 +4,10 @@
             James Peachey, HEASARC
 */
 
-#include <math.h>
+#include <algorithm>
 #include <iostream>
 #include <stdexcept>
 #include <string>
-#include <vector>
 
 // Parameter file access from st_app.
 #include "st_app/AppParGroup.h"
@@ -23,9 +22,11 @@
 #include "tip/Table.h"
 
 // Binners used:
-#include "evtbin/TipBinner2D.h"
-#include "evtbin/TipEqualLinearBinner.h"
-#include "evtbin/TipEqualLogBinner.h"
+#include "evtbin/Hist1D.h"
+#include "evtbin/Hist2D.h"
+#include "evtbin/LinearBinner.h"
+#include "evtbin/LogBinner.h"
+#include "evtbin/RecordBinFiller.h"
 
 /** \class EvtBin
     \brief Application singleton for evtbin.
@@ -50,10 +51,12 @@ class EvtBin : public st_app::StApp {
       if (0 == algorithm.compare("LC")) makeLightCurve(pars);
       else if (0 == algorithm.compare("PHA2")) makePha2(pars);
       else if (0 == algorithm.compare("SPEC")) makeSimpleSpectrum(pars);
-      else throw std::logic_error("Only Light Curve is currently supported");
+      else throw std::logic_error(std::string("Algorithm ") + algorithm + " is not supported");
     }
 
     void makeSimpleSpectrum(const st_app::AppParGroup & pars) {
+      using namespace evtbin;
+
       std::string event_file = pars["eventfile"];
       std::string out_file = pars["outfile"];
       
@@ -73,17 +76,30 @@ class EvtBin : public st_app::StApp {
       // Make sure a positive number of bins was entered.
       if (0 >= enum_bins) throw std::runtime_error("Number of bins (enumbins parameter) must be positive");
 
-      // Create binner object:
-      TipEqualLogBinner binner("ENERGY", "COUNTS", energy_min, energy_max, (unsigned long)(enum_bins));
+      // Create histogram:
+      Hist1D hist(LogBinner(energy_min, energy_max, enum_bins, "ENERGY"));
 
       // Fill the histogram:
-      binner.fillBins(table->begin(), table->end());
+      std::for_each(table->begin(), table->end(), RecordBinFiller(hist));
 
       // Open output table for writing:
       tip::Table * out_table = tip::IFileSvc::instance().editTable(out_file, "SPECTRUM");
 
-      // Write the output table:
-      binner.writeHistogram(out_table);
+      // Get the histogram's binner.
+      const Binner * binner = hist.getBinners()[0];
+
+      // From the binner, get the number of bins.
+      long num_bins = binner->getNumBins();
+
+      // Set size of output table such that it can accomodate these bins.
+      out_table->setNumRecords(num_bins);
+
+      // Write the output table.
+      tip::Table::Iterator table_itor = out_table->begin();
+      for (long index = 0; index != num_bins; ++index, ++table_itor) {
+        (*table_itor)["CHANNEL"].set(index + 1);
+        (*table_itor)["COUNTS"].set(hist[index]);
+      }
 
 // TODO:
 // 1. Energy units: Xspec needs keV, input is MeV but could be anything (read keyword)
@@ -93,6 +109,8 @@ class EvtBin : public st_app::StApp {
     }
 
     void makeLightCurve(const st_app::AppParGroup & pars) {
+      using namespace evtbin;
+
       std::string event_file = pars["eventfile"];
       std::string out_file = pars["outfile"];
 
@@ -109,17 +127,32 @@ class EvtBin : public st_app::StApp {
       double tstop = pars["tstop"];
       double width = pars["deltatime"];
 
-      // Create binner object:
-      TipEqualLinearBinner binner("TIME", "COUNTS", tstart, tstop, width);
+      // Create histogram:
+// TODO LinearBinner needs another constructor with the width specified, and needs to use this width...
+      Hist1D hist(LinearBinner(tstart, tstop, long((tstop - tstart)/width), "TIME"));
 
       // Fill the histogram:
-      binner.fillBins(table->begin(), table->end());
+      std::for_each(table->begin(), table->end(), RecordBinFiller(hist));
 
       // Open output table for writing:
       tip::Table * out_table = tip::IFileSvc::instance().editTable(out_file, "RATE");
 
-      // Write the output table:
-      binner.writeHistogram(out_table);
+      // Get the histogram's binner.
+      const Binner * binner = hist.getBinners()[0];
+
+      // From the binner, get the number of bins.
+      long num_bins = binner->getNumBins();
+
+      // Set size of output table such that it can accomodate these bins.
+      out_table->setNumRecords(num_bins);
+
+      // Write the output table.
+      tip::Table::Iterator table_itor = out_table->begin();
+      for (long index = 0; index != num_bins; ++index, ++table_itor) {
+        (*table_itor)["TIME"].set(binner->getInterval(index).midpoint());
+        (*table_itor)["TIMEDEL"].set(binner->getInterval(index).width());
+        (*table_itor)["COUNTS"].set(hist[index]);
+      }
 
 // TODO for light curve:
 // 1. Copy GTI from event file (anding with the range [tstart, tstop])
@@ -133,6 +166,8 @@ class EvtBin : public st_app::StApp {
     }
 
     void makePha2(const st_app::AppParGroup & pars) {
+      using namespace evtbin;
+
       std::string event_file = pars["eventfile"];
       std::string out_file = pars["outfile"];
       
@@ -158,20 +193,42 @@ class EvtBin : public st_app::StApp {
       if (0 >= enum_bins) throw std::runtime_error("Number of bins (enumbins parameter) must be positive");
 
       // Create one-dim binner objects:
-      TipEqualLinearBinner time_binner("TIME", "COUNTS", tstart, tstop, width);
-      TipEqualLogBinner energy_binner("ENERGY", "COUNTS", energy_min, energy_max, (unsigned long)(enum_bins));
+//      TipEqualLinearBinner time_binner("TIME", "COUNTS", tstart, tstop, width);
+//      TipEqualLogBinner energy_binner("ENERGY", "COUNTS", energy_min, energy_max, (unsigned long)(enum_bins));
+
+      Hist2D hist(
+        LinearBinner(tstart, tstop, long((tstop - tstart)/width), "TIME"),
+        LogBinner(energy_min, energy_max, enum_bins, "ENERGY")
+      );
 
       // Create two-dim gestalt binner which uses both the above binners:
-      TipBinner2D binner(&time_binner, &energy_binner);
+//      TipBinner2D binner(&time_binner, &energy_binner);
 
       // Fill the histogram:
-      binner.fillBins(table->begin(), table->end());
+//      binner.fillBins(table->begin(), table->end());
+      std::for_each(table->begin(), table->end(), RecordBinFiller(hist));
 
       // Open output table for writing:
       tip::Table * out_table = tip::IFileSvc::instance().editTable(out_file, "SPECTRUM");
 
       // Write the output table:
-      binner.writeHistogram(out_table);
+//      binner.writeHistogram(out_table);
+
+      // Get the histogram's energy binner.
+      const Binner * binner = hist.getBinners()[1];
+
+      // From the binner, get the number of bins.
+      long num_bins = binner->getNumBins();
+
+      // Set size of output table such that it can accomodate these bins.
+      out_table->setNumRecords(num_bins);
+
+      // Write the output table.
+      tip::Table::Iterator table_itor = out_table->begin();
+      for (long index = 0; index != num_bins; ++index, ++table_itor) {
+        (*table_itor)["CHANNEL"].set(index + 1);
+        (*table_itor)["COUNTS"].set(hist[0][index]);
+      }
 
 // TODO:
 // 1. Energy units: Xspec needs keV, input is MeV but could be anything (read keyword)
