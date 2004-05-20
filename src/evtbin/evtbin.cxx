98f48a9b1ab25ae2f887b19f067ae065e09a927d
peachey <>
2004-05-20 19:16:23 +0000

    Refactor to remove redundancy and take advantage of DataProduct class family.

--- src/evtbin/evtbin.cxx
+++ src/evtbin/evtbin.cxx
@@ -1,17 +1,25 @@
 /** \file EvtBin.cxx
-    \brief Event binning executable.
+    \brief Event binning executable. Really this is a shell application whose run() method creates and
+    runs one of several specialized binning applications.
     \author Yasushi Ikebe, GSSC
             James Peachey, HEASARC
 */
-
-#include <algorithm>
-#include <iostream>
 #include <stdexcept>
 #include <string>
 
-// Parameter file access from st_app.
+// Binners used:
+#include "evtbin/LinearBinner.h"
+#include "evtbin/LogBinner.h"
+
+// Data product support classes.
+#include "evtbin/DataProduct.h"
+#include "evtbin/LightCurve.h"
+#include "evtbin/MultiSpec.h"
+#include "evtbin/SingleSpec.h"
+
+// Interactive parameter file access from st_app.
 #include "st_app/AppParGroup.h"
-// Application base class.
+// Science Tools Application base class.
 #include "st_app/StApp.h"
 // Factory used by st_app's standard main to create application object.
 #include "st_app/StAppFactory.h"
@@ -21,12 +29,179 @@
 // Table access from tip.
 #include "tip/Table.h"
 
-// Binners used:
-#include "evtbin/Hist1D.h"
-#include "evtbin/Hist2D.h"
-#include "evtbin/LinearBinner.h"
-#include "evtbin/LogBinner.h"
-#include "evtbin/RecordBinFiller.h"
+/** \class EvtBinAppBase
+    \brief Base class for specific binning applications. This has a generic run() method which is valid for
+    all binning applications. The logic of run() is a good place to start to understand how the pieces fit together.
+*/
+class EvtBinAppBase : public st_app::StApp {
+  public:
+    /** \brief Construct a binning application with the given name.
+        \param app_name the name of the application.
+    */
+    EvtBinAppBase(const std::string & app_name): m_app_name(app_name) {}
+
+    virtual ~EvtBinAppBase() throw() {}
+
+    /** \brief Standard "main" for an event binning application. This is the standard recipe for binning,
+        with steps which vary between specific apps left to subclasses to define.
+    */
+    virtual void run() {
+      using namespace evtbin;
+
+      // Get parameter file object.
+      st_app::AppParGroup & pars = getParGroup(m_app_name);
+
+      // Prompt for parameters necessary for this application.
+      parPrompt(pars);
+
+      // Save all parameters from this tool run now.
+      pars.Save();
+
+      const tip::Table * events = 0;
+      DataProduct * product = 0;
+      
+      try {
+        // Open input file for reading only.
+        events = tip::IFileSvc::instance().readTable(pars["eventfile"], "EVENTS");
+
+        // Get data object.
+        product = createDataProduct(pars);
+
+        // Copy keywords from input events table.
+        product->harvestKeywords(events->getHeader());
+
+        // Bin input data into product.
+        product->binInput(events->begin(), events->end());
+
+        // Write the data object output.
+        product->writeOutput(m_app_name, pars["outfile"]);
+
+      } catch (...) {
+        delete product;
+        delete events;
+        throw;
+      }
+      delete product;
+      delete events;
+    }
+
+    /** \brief Prompt for all parameters needed by a particular binner. The base class version prompts
+        for universally needed parameters.
+        \param pars The parameter prompting object.
+    */
+    virtual void parPrompt(st_app::AppParGroup & pars) {
+      //
+      pars.Prompt("eventfile");
+      pars.Prompt("outfile");
+    }
+
+    /** \brief Create a specific data product object using the given parameters.
+        \param pars The parameter prompting object.
+    */
+    virtual evtbin::DataProduct * createDataProduct(const st_app::AppParGroup & pars) = 0;
+
+  private:
+    std::string m_app_name;
+};
+
+/** \class LightCurveApp
+    \brief Light curve specific binning application.
+*/
+class LightCurveApp : public EvtBinAppBase {
+  public:
+    LightCurveApp(const std::string & app_name): EvtBinAppBase(app_name) {}
+
+    virtual void parPrompt(st_app::AppParGroup & pars) {
+      // Call base class prompter for standard universal parameters.
+      EvtBinAppBase::parPrompt(pars);
+
+      // Prompt for remaining parameters needed for product.
+      pars.Prompt("tstart");
+      pars.Prompt("tstop");
+      pars.Prompt("deltatime");
+    }
+
+    virtual evtbin::DataProduct * createDataProduct(const st_app::AppParGroup & pars) {
+      using namespace evtbin;
+
+      // Get ranges:
+      double tstart = pars["tstart"];
+      double tstop = pars["tstop"];
+      double width = pars["deltatime"];
+
+      // Create data object.
+      return new LightCurve(LinearBinner(tstart, tstop, long((tstop - tstart) / width), "TIME"));
+    }
+};
+
+/** \class MultiSpectraApp
+    \brief Multiple spectra-specific binning application.
+*/
+class MultiSpectraApp : public EvtBinAppBase {
+  public:
+    MultiSpectraApp(const std::string & app_name): EvtBinAppBase(app_name) {}
+
+    virtual void parPrompt(st_app::AppParGroup & pars) {
+      // Call base class prompter for standard universal parameters.
+      EvtBinAppBase::parPrompt(pars);
+
+      // Prompt for remaining parameters needed for product.
+      pars.Prompt("emin");
+      pars.Prompt("emax");
+      pars.Prompt("enumbins");
+
+      pars.Prompt("tstart");
+      pars.Prompt("tstop");
+      pars.Prompt("deltatime");
+    }
+
+    virtual evtbin::DataProduct * createDataProduct(const st_app::AppParGroup & pars) {
+      using namespace evtbin;
+
+      // Get ranges:
+      double energy_min = pars["emin"];
+      double energy_max = pars["emax"];
+      double energy_num_bins = pars["enumbins"];
+
+      double tstart = pars["tstart"];
+      double tstop = pars["tstop"];
+      double width = pars["deltatime"];
+
+      // Create data object.
+      return new MultiSpec obj(LinearBinner(tstart, tstop, long((tstop - tstart)/width), "TIME"),
+        LogBinner(energy_min, energy_max, enum_bins, "ENERGY"));
+    }
+};
+
+/** \class SimpleSpectrumApp
+    \brief Single spectrum-specific binning application.
+*/
+class SimpleSpectrumApp : public EvtBinAppBase {
+  public:
+    SimpleSpectrumApp(const std::string & app_name): EvtBinAppBase(app_name) {}
+
+    virtual void parPrompt(st_app::AppParGroup & pars) {
+      // Call base class prompter for standard universal parameters.
+      EvtBinAppBase::parPrompt(pars);
+
+      // Prompt for remaining parameters needed for light curve.
+      pars.Prompt("emin");
+      pars.Prompt("emax");
+      pars.Prompt("enumbins");
+    }
+
+    virtual evtbin::DataProduct * createDataProduct(const st_app::AppParGroup & pars) {
+      using namespace evtbin;
+
+      // Get ranges:
+      double energy_min = pars["emin"];
+      double energy_max = pars["emax"];
+      double energy_num_bins = pars["enumbins"];
+
+      // Create data object.
+      return new SingleSpectrum(LogBinner(energy_min, energy_max, energy_num_bins, "ENERGY"));
+    }
+};
 
 /** \class EvtBin
     \brief Application singleton for evtbin.
@@ -41,118 +216,32 @@ class EvtBin : public st_app::StApp {
       // Get parameter file object:
       st_app::AppParGroup & pars = getParGroup("evtbin");
 
-      // Prompt for all parameters:
-      pars.Prompt();
-
-      // Save values of parameters:
+      // Prompt for algorithm parameter, which determines which application is really used.
+      pars.Prompt("algorithm");
       pars.Save();
 
       std::string algorithm = pars["algorithm"];
-      if (0 == algorithm.compare("LC")) makeLightCurve(pars);
-      else if (0 == algorithm.compare("PHA2")) makePha2(pars);
-      else if (0 == algorithm.compare("SPEC")) makeSimpleSpectrum(pars);
-      else throw std::logic_error(std::string("Algorithm ") + algorithm + " is not supported");
+
+      // Based on this parameter, create the real application.
+      st_app::StApp * app = 0;
+
+      try {
+        if (0 == algorithm.compare("LC")) app = new LightCurveApp("evtbin");
+        else if (0 == algorithm.compare("PHA2")) app = new MultiSpectraApp("evtbin");
+        else if (0 == algorithm.compare("SPEC")) app = new SimpleSpectrumApp("evtbin");
+        else throw std::logic_error(std::string("Algorithm ") + algorithm + " is not supported");
+
+        // Run the real application.
+        app->run();
+      } catch (...) {
+        delete app;
+        throw;
+      }
+      delete app;
     }
 
-    void makeSimpleSpectrum(const st_app::AppParGroup & pars) {
-      using namespace evtbin;
+};
 
-      std::string event_file = pars["eventfile"];
-      std::string out_file = pars["outfile"];
-      
-      // Get the IFileSvc singleton and use it to create the output file:
-      // tip == namespace, IFileSvc == class, instance() is static method returning the singleton
-      // createFile is a normal method:
-      tip::IFileSvc::instance().createFile(out_file, "LatSingleBinnedTemplate");
- 
-      // Open input file for reading only:
-      const tip::Table * table = tip::IFileSvc::instance().readTable(event_file, "EVENTS");
-
-      // Now get ranges:
-      double energy_min = pars["emin"];
-      double energy_max = pars["emax"];
-      long enum_bins = pars["enumbins"];
-
-      // Make sure a positive number of bins was entered.
-      if (0 >= enum_bins) throw std::runtime_error("Number of bins (enumbins parameter) must be positive");
-
-      // Create histogram:
-      Hist1D hist(LogBinner(energy_min, energy_max, enum_bins, "ENERGY"));
-
-      // Fill the histogram:
-      std::for_each(table->begin(), table->end(), RecordBinFiller(hist));
-
-      // Open output table for writing:
-      tip::Table * out_table = tip::IFileSvc::instance().editTable(out_file, "SPECTRUM");
-
-      // Get the histogram's binner.
-      const Binner * binner = hist.getBinners()[0];
-
-      // From the binner, get the number of bins.
-      long num_bins = binner->getNumBins();
-
-      // Set size of output table such that it can accomodate these bins.
-      out_table->setNumRecords(num_bins);
-
-      // Write the output table.
-      tip::Table::Iterator table_itor = out_table->begin();
-      for (long index = 0; index != num_bins; ++index, ++table_itor) {
-        (*table_itor)["CHANNEL"].set(index + 1);
-        (*table_itor)["COUNTS"].set(hist[index]);
-      }
-
-// TODO:
-// 1. Energy units: Xspec needs keV, input is MeV but could be anything (read keyword)
-      delete out_table;
-
-      delete table;
-    }
-
-    void makeLightCurve(const st_app::AppParGroup & pars) {
-      using namespace evtbin;
-
-      std::string event_file = pars["eventfile"];
-      std::string out_file = pars["outfile"];
-
-      // Get the IFileSvc singleton and use it to create the output file:
-      // tip == namespace, IFileSvc == class, instance() is static method returning the singleton
-      // createFile is a normal method:
-      tip::IFileSvc::instance().createFile(out_file, "LatLightCurveTemplate");
- 
-      // Open input file for reading only:
-      const tip::Table * table = tip::IFileSvc::instance().readTable(event_file, "EVENTS");
-
-      // Now get ranges:
-      double tstart = pars["tstart"];
-      double tstop = pars["tstop"];
-      double width = pars["deltatime"];
-
-      // Create histogram:
-// TODO LinearBinner needs another constructor with the width specified, and needs to use this width...
-      Hist1D hist(LinearBinner(tstart, tstop, long((tstop - tstart)/width), "TIME"));
-
-      // Fill the histogram:
-      std::for_each(table->begin(), table->end(), RecordBinFiller(hist));
-
-      // Open output table for writing:
-      tip::Table * out_table = tip::IFileSvc::instance().editTable(out_file, "RATE");
-
-      // Get the histogram's binner.
-      const Binner * binner = hist.getBinners()[0];
-
-      // From the binner, get the number of bins.
-      long num_bins = binner->getNumBins();
-
-      // Set size of output table such that it can accomodate these bins.
-      out_table->setNumRecords(num_bins);
-
-      // Write the output table.
-      tip::Table::Iterator table_itor = out_table->begin();
-      for (long index = 0; index != num_bins; ++index, ++table_itor) {
-        (*table_itor)["TIME"].set(binner->getInterval(index).midpoint());
-        (*table_itor)["TIMEDEL"].set(binner->getInterval(index).width());
-        (*table_itor)["COUNTS"].set(hist[index]);
-      }
 
 // TODO for light curve:
 // 1. Copy GTI from event file (anding with the range [tstart, tstop])
@@ -160,84 +249,9 @@ class EvtBin : public st_app::StApp {
 // 3. Need different template which has no TIME_DEL column in uniform case.
 // 4. Livetime (applies for all binners) from ft2 file gets written in header of histogram.
 //    Recompute iff GTI changed (in step 1).
-      delete out_table;
 
-      delete table;
-    }
-
-    void makePha2(const st_app::AppParGroup & pars) {
-      using namespace evtbin;
-
-      std::string event_file = pars["eventfile"];
-      std::string out_file = pars["outfile"];
-      
-      // Get the IFileSvc singleton and use it to create the output file:
-      // tip == namespace, IFileSvc == class, instance() is static method returning the singleton
-      // createFile is a normal method:
-      tip::IFileSvc::instance().createFile(out_file, "LatSingleBinnedTemplate");
- 
-      // Open input file for reading only:
-      const tip::Table * table = tip::IFileSvc::instance().readTable(event_file, "EVENTS");
-
-      // Now get time ranges:
-      double tstart = pars["tstart"];
-      double tstop = pars["tstop"];
-      double width = pars["deltatime"];
-
-      // Now get energy ranges:
-      double energy_min = pars["emin"];
-      double energy_max = pars["emax"];
-      long enum_bins = pars["enumbins"];
-
-      // Make sure a positive number of bins was entered.
-      if (0 >= enum_bins) throw std::runtime_error("Number of bins (enumbins parameter) must be positive");
-
-      // Create one-dim binner objects:
-//      TipEqualLinearBinner time_binner("TIME", "COUNTS", tstart, tstop, width);
-//      TipEqualLogBinner energy_binner("ENERGY", "COUNTS", energy_min, energy_max, (unsigned long)(enum_bins));
-
-      Hist2D hist(
-        LinearBinner(tstart, tstop, long((tstop - tstart)/width), "TIME"),
-        LogBinner(energy_min, energy_max, enum_bins, "ENERGY")
-      );
-
-      // Create two-dim gestalt binner which uses both the above binners:
-//      TipBinner2D binner(&time_binner, &energy_binner);
-
-      // Fill the histogram:
-//      binner.fillBins(table->begin(), table->end());
-      std::for_each(table->begin(), table->end(), RecordBinFiller(hist));
-
-      // Open output table for writing:
-      tip::Table * out_table = tip::IFileSvc::instance().editTable(out_file, "SPECTRUM");
-
-      // Write the output table:
-//      binner.writeHistogram(out_table);
-
-      // Get the histogram's energy binner.
-      const Binner * binner = hist.getBinners()[1];
-
-      // From the binner, get the number of bins.
-      long num_bins = binner->getNumBins();
-
-      // Set size of output table such that it can accomodate these bins.
-      out_table->setNumRecords(num_bins);
-
-      // Write the output table.
-      tip::Table::Iterator table_itor = out_table->begin();
-      for (long index = 0; index != num_bins; ++index, ++table_itor) {
-        (*table_itor)["CHANNEL"].set(index + 1);
-        (*table_itor)["COUNTS"].set(hist[0][index]);
-      }
-
-// TODO:
+// TODO for Spectra
 // 1. Energy units: Xspec needs keV, input is MeV but could be anything (read keyword)
-      delete out_table;
-
-      delete table;
-    }
-
-};
 
 /// \brief Create factory object which can create the application:
 st_app::StAppFactory<EvtBin> g_app_factory;
