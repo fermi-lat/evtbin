b1816af99459c3a8e1f4d82205de87dceb5c56e7
peachey <>
2004-05-20 19:19:06 +0000

    Update documentation for v0r2p0.

--- src/mainpage.h
+++ src/mainpage.h
@@ -6,12 +6,12 @@
 
     \section intro Introduction
     This package consists of a class library and an application. The
-    library contains abstractions of histograms and algorithms which
-    bin collections of data values into histograms. The abstractions
-    in the library are generic, thus allowing them to be used for
-    different and disparate applications. Also included is an interface
-    to allow these histograms to be used easily in tandem with the
-    tip library, to facilitate binning of data in files.
+    library contains abstractions which facilitate
+    binning collections of data values into histograms. The abstractions
+    in the library are organized in layers ranging from generic binners
+    ans histograms to representations of more specific higher level data
+    types such as light curves, with file access via Tip. This is to allow
+    the library to be used in maximally different and disparate contexts.
 
     The evtbin application is considerably more specialized. It
     operates on an input event file (and other optional input files
@@ -49,7 +49,7 @@
                Application: 5) Refactor to use library to accomplish
                the most critical analysis goals: light curve,
                time-unbinned spectra (PHA1), time-binned spectra
-               (PHA1 and PHA2). Critical point: files produced
+               (PHA2). Critical point: files produced
                must pass fverify, and comply with the letter of
                OGIP standards for their respective file formats.
 
@@ -76,40 +76,64 @@
                5 has been partly addressed. The PHA2 case does not
                work correctly, mainly because there is not yet a
                template for PHA2 files.
+
+    5/20/2004  Point 5 has now been completely addressed. This version was tagged v0r2p0.
 \endverbatim
 
-    \section details Library Design Details
-    Base classes for families include Binner and Hist. The Binner
-    hierarchy provides abstractions to manage bin definitions, and
-    to determine the bin index for any given data value. However
-    binners do not store any data. That is the job of histograms
-    in the Hist hierarchy. Hist objects use Binner in a Strategy
-    pattern: a Hist object is configured with a supplied Binner
-    subobject, allowing arbitrary binning methods for each dimension
-    of a histogram.
+    \section library_details Library Design Details
+    There are currently three families used in the library.
+    They are, in increasing orders of abstraction, Binner, Hist
+    and DataProduct.
 
-    In addition, a RecordBinFiller functor exists for the purpose of
-    filling a given Hist object from a table record from the tip
-    package. Beyond this, tip-specific writing classes have not
-    yet been added because writing output is pretty heterogeneous,
-    even in very similar situations. For example, when writing a light
-    curve, the interval information goes directly in the output table,
-    while for spectra the intervals are described in the EBOUNDS
-    extension. Similarly, the CHANNEL field is written for spectra,
-    but not for light curves. For the moment, application will simply
-    maintain different methods for each file output type. Such methods
-    may be moved to the library at some future point.
+    \subsection binner The Binner Hierarchy
+    Binner and its subclasses encapsulate the notion of binning
+    in one dimension. Subclasses correspond to particular types
+    of binners, e.g. LinearBinner, LogBinner, etc. These classes
+    store information pertaining to the bins, but they do not themselves
+    store any binned data.
+
+    \subsection hist The Hist Hierarchy
+    Hist and its subclasses encapsulate various dimensionalities of
+    histograms. Hist and its subclasses contain binned data, but use
+    Binner objects in a Strategy pattern to determine which bin should
+    contain input values. For example, Hist2D is a
+    histogram with two binning dimensions. It contains a two dimensional
+    array to store the binned values, and two Binner objects which determine
+    the indices in the array for a given input value.
+
+    \subsection data_product The DataProduct Hierarchy
+    DataProduct and its subclasses are higher level abstractions which
+    correspond to specific histogram-based applications. In addition
+    to a histogram aspect, DataProduct provides file access using
+    Tip. For example, the LightCurve subclass is a front end to a
+    one dimensional histogram, which can be filled from an arbitrary
+    tip::Table. In addition, LightCurve has an output method which
+    can write a legal OGIP-compliant light curve FITS file using Tip.
+
+    \section app_details Application Details
+    For details of the design of the application, see comments in
+    evtbin.cxx.
 
     \section todo Open Issues
 \verbatim
-       1. 5/13/2004: PHA2 case doesn't work because template file is not available.
-       2. 5/13/2004: Keywords in output file are wrong in many cases (e.g. DATE-OBS)
-          because a means does not exist to update them from the input file.
-       3. 5/13/2004: The main application, evtbin, has some redundant (parallel) code
-          for different similar binning situations. This should naturally be consolidated.
 \endverbatim
 
     \section done Resolved Issues
 \verbatim
+       1. 5/13/2004: PHA2 case doesn't work because template file is not available.
+          5/20/2004: Resolved. Template file LatBinnedTemplate is actually the correct
+          template for PHA2, but the template file has 1 element in each column which
+          is supposed to be a vector with multiple elements. To address this,Tip was modified
+          to support changing the number of elements in a vector column. This is now
+          implemented in the MultiSpec class.
+       2. 5/13/2004: Keywords in output file are wrong in many cases (e.g. DATE-OBS)
+          because a means does not exist to update them from the input file.
+          5/20/2004: Resolved. The harvestKeywords method in DataProduct fills a container
+          with keyword values from a tip::Table. These keywords are subsequently used by
+          writeOutput to update keywords in the output file.
+       3. 5/13/2004: The main application, evtbin, has some redundant (parallel) code
+          for different similar binning situations. This should naturally be consolidated.
+          5/20/2004: Resolved. A helper class hierarchy EvtBinAppBase was developed to
+          allow distinct binning applications to share the common bits of their algorithms.
 \endverbatim
 */
