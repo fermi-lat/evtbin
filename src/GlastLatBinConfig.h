d4537ce77523daab9346da735b8a869bcd97659a
peachey <>
2005-03-06 19:43:27 +0000

    Add mission/instrument-dependent configuration classes for LAT and GBM.

---
+++ src/GlastLatBinConfig.h
@@ -0,0 +1,26 @@
+/** \file GlastLatBinConfig.h
+    \brief Helper class which uses standard sets of parameters to configure binners for standard applications.
+    \author James Peachey, HEASARC
+*/
+#ifndef evtbin_GlastLatBinConfig_h
+#define evtbin_GlastLatBinConfig_h
+
+#include "evtbin/BinConfig.h"
+
+namespace evtbin {
+
+  class Binner;
+
+  /** \class GlastLatBinConfig
+      \brief Helper class which uses standard sets of parameters to configure binners for standard applications.
+  */
+  class GlastLatBinConfig : public BinConfig {
+    public:
+      static void load();
+
+      virtual GlastLatBinConfig * clone() const;
+  };
+
+}
+
+#endif
