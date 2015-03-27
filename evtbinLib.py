c14d5988a2e788b10592c896128511482b28f130
echarles <>
2015-03-27 20:39:04 +0000

    add dependence on HEALPix external

--- evtbinLib.py
+++ evtbinLib.py
@@ -1,4 +1,4 @@
-#$Id: evtbinLib.py,v 1.2 2008/02/26 03:10:49 glastrm Exp $
+#$Id: evtbinLib.py,v 1.4 2015/03/05 19:58:35 echarles Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly',0):
         env.Tool('addLibrary', library = ['evtbin'])
@@ -7,7 +7,8 @@ def generate(env, **kw):
     env.Tool('st_facilitiesLib')
     env.Tool('st_streamLib')
     env.Tool('tipLib')
-    env.Tool('healpixLib')
+    # EAC, add dependence on HEALPix external
+    env.Tool('addLibrary', library=env['healpixlibs'])
 
 def exists(env):
     return 1
